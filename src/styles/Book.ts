8d6d404446728a5fb9d103f26d1ef7f7bf5a64ef
JohnWick92 <grumec92@gmail.com>
2022-12-08 09:41:15 -0300

    Styles for book preview card

---
+++ src/styles/Book.ts
@@ -0,0 +1,19 @@
+import { styled } from '@stitches/react'
+
+export const BookCard = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: '20% 80%',
+  color: 'Black',
+  width: '80%',
+  height: '200px',
+  border: '1px solid grey',
+  borderRadius: '8px',
+  background: 'white',
+  marginInline: '10%',
+  textAlign: 'left',
+  padding: '12px 12px',
+})
+
+export const Img = styled('div', {
+  paddingLeft: '10px',
+})
